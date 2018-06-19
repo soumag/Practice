682955d1f7c994132a5a58f6c1544f093def7277
Soumil Agarwal <soagarwa@microsoft.com>
2018-06-19 16:55:46 +0530

    Leetcode invert binary tree solution

---
+++ Solution226.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_Practise
+{
+    class Solution226
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;            
+        }
+    }
+}
