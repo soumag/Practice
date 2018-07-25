44b50541191e1ea05b349d260e9a37638d412c0f
Soumil Agarwal <soagarwa@microsoft.com>
2018-07-25 16:19:00 +0530

    Chaging script

--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # GNU bash, version 4.3.46
-echo "Hello, world!" >> /tmp/test.txt
+sudo apt install unzip                     && \
+sudo apt-get --assume-yes install npm      && \
+mkdir /tmp/downloadedApp                   && \
+cd /tmp/downloadedApp                      && \
+wget $1 -o app.zip                         && \
+unzip app.zip                              && \
+rm app.zip                                 && \
+npm i                                      && \
+npm start
